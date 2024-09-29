a048124626e2f09e6e7ef809d63738a4b7abe83b
Patrick Louie Policarpio <patricklouie.pl@gmail.com>
2024-09-30 07:33:13 +0800

    Improved kingdom

--- app/components/Contents/Kingdom.tsx
+++ app/components/Contents/Kingdom.tsx
@@ -1,31 +1,15 @@
 import React from "react";
 import "@/app/styles/pages/kingdom.css";
-import KingdomProfile from "../elements/Kingdom/KingdomProfile";
-import KingdomExperience from "../elements/Kingdom/KingdomExperience";
-import KingdomItems from "../elements/Kingdom/KingdomItems";
-import { Button } from "@nextui-org/button";
+import Image from "next/image";
 
 const Kingdom = () => {
     return (
         <div className="main-kingdom-container">
-            <KingdomProfile></KingdomProfile>
-            <div style={{ marginBottom: "10px" }}></div>
-            <KingdomExperience></KingdomExperience>
-            <div style={{ marginBottom: "20px" }}></div>
-            <Button className="upgrade-button box-button base-text">Upgrade</Button>
-            <div style={{ marginBottom: "40px" }}></div>
-            Kingdom Buildings
-            <div style={{ marginBottom: "10px" }}></div>
-            <div className="kingdoms-container">
-                <KingdomItems level={1} name="Main Castle" />
-                <KingdomItems level={1} name="Main Castle" /> 
-                <KingdomItems level={1} name="Main Castle" /> 
-                <KingdomItems level={1} name="Main Castle" /> 
-                <KingdomItems level={1} name="Main Castle" /> 
-                <KingdomItems level={1} name="Main Castle" /> 
-                <KingdomItems level={1} name="Main Castle" /> 
-                <KingdomItems level={1} name="Main Castle" /> 
+            <div className="flex pb-5">
+                <div className="curved-box profit base-text">Profit (1H)</div>
+                <div className="curved-box build base-text">Build</div>
             </div>
+            <Image src="/assets/images/kingdoms/kingdom.png" alt="kingdom" width={1000} height={1000}/>
         </div>
     );
 };
