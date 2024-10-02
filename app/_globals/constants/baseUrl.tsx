3dd45d6668d0ce9d1072f8806506f153fafe86d5
Vonn <vonncc123@gmail.com>
2024-10-02 10:53:35 +0800

    changed to correct endpoint

--- app/_globals/constants/baseUrl.tsx
+++ app/_globals/constants/baseUrl.tsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.dev.avalorions.io';
 
 export const API_ENDPOINTS = {
 
