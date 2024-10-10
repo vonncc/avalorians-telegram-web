8134ef12211b63d9591cb291c8db01e30a3833a7
jjbc-ocm <jjbconecodeman@gmail.com>
2024-10-10 15:29:54 +0800

    Load webgl scene test

--- app/components/Contents/Lane.tsx
+++ app/components/Contents/Lane.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import "@/app/styles/pages/lane.css";
 
-const Lane = () => {
+const Lane = ({ token }) => {
     const iframeRef = useRef(null);
 
     useEffect(() => {
@@ -14,16 +14,17 @@ const Lane = () => {
                 return;
             }
 
-            const data = JSON.parse(event.data);
+            const data = typeof event.data === "object" ? event.data : JSON.parse(event.data);
             console.log("Received message from Unity WebGL:", data);
 
             if (data.action === "initialization") {
-                
+                sendMessageToUnity("access_token", token);
+                sendMessageToUnity("scene_change", "lane");
             }
         };
 
         // Add event listener for message events
-        window.addEventListener("message", handleMessage);
+        window.addEventListener("message", handleMessage); 
 
         // Clean up the event listener when the component unmounts
         return () => {
@@ -32,12 +33,15 @@ const Lane = () => {
     }, []);
 
     // Function to send a message to the Unity WebGL iframe
-    const sendMessageToUnity = (message) => {
+    const sendMessageToUnity = (action, content) => {
         const iframe = iframeRef.current;
 
         if (iframe && iframe.contentWindow) {
-            iframe.contentWindow.postMessage(message, window.location.origin);
-            console.log("Sent message to Unity WebGL:", message);
+            const data = {
+                action: action,
+                content: content
+            };
+            iframe.contentWindow.postMessage(JSON.stringify(data), window.location.origin);
         } else {
             console.error("Iframe not found or not loaded yet.");
         }
